833190600ddb1aae02ba9a243759db74317ea65d
jun3t <junet_kharisma@yahoo.com>
2022-05-28 15:51:53 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,11 +1,69 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+
+const firstUserColor = new Set(['Yellow', 'Pink', 'White', 'Purple']);
+const arrFirstUserColor = Array.from(firstUserColor);
+
+const firstUserRestaurant = new Set(['Bento', 'Sushi', 'Pancake', 'Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi']);
+const arrFirstUserRestaurant = Array.from(firstUserRestaurant);
+
+const secondUserColor = new Set(['Blue', 'Black', 'Grey'])
+const arrSecondUserColor = Array.from(secondUserColor);
+
+const secondUserRestaurant =  new Set(['Tempura', 'Bento', 'Sushi', 'Pancake', 'Padang', 'Katsu', 'Geprek', 'Pancake', 'Eggy']);
+const arrSecondUserRestaurant = Array.from(secondUserRestaurant);
+
+const firstUser = {
+    name: 'Monica',
+    gender: 'Female',
+    age: 17,
+    email: 'monica@dingdong.com',
+    favoriteColor: arrFirstUserColor,
+    isHavePet: true,
+    education: [{
+        name: 'SD 01', 
+        city: 'Jakarta', 
+        graduate: '2016'
+    }, {
+        name: 'SMP 02', 
+        city: 'Jakarta', 
+        graduate: '2019'
+    },{
+        name: 'SMA 03', 
+        city: 'Tangerang'
+    }],
+    favoriteRestaurant: arrFirstUserRestaurant
+};
+const secondUser = {
+    name: 'Wendy',
+    gender: 'Male',
+    age: 23,
+    email: 'wendy@dingdong.com',
+    favoriteColor: arrSecondUserColor,
+    isHavePet: false,
+    education: [{
+        name: 'SD 02', 
+        city: 'Jakarta', 
+        graduate: '2010'
+    }, {
+        name: 'SMP 03', 
+        city: 'Bogor', 
+        graduate: '2013'
+    },{
+        name: 'SMA 01', 
+        city: 'Surabaya', 
+        graduate: '2016'
+    },{
+        name: 'Universitas Maju',
+        city: 'Tangerang'
+    }],
+    favoriteRestaurant: arrSecondUserRestaurant
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
+users.push(firstUser, secondUser);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
